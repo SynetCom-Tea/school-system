bc3638fcef95e858b2da40e035c27de16c6ca2c9
inz2020 <72919182+inz2020@users.noreply.github.com>
2023-08-17 18:02:18 +0100

    Create new vue files

---
+++ resources/js/pages/welcome/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Hello welcome</h1>
+  </div>
+  <v-divider></v-divider>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: [],
+  data() {
+    return {};
+  },
+  computed: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped></style>
