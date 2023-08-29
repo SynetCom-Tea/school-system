3ae5a3768da7ff9a36d35d4c5330bfc6427a7cba
Momlaouan <momlaouali@gmail.com>
2023-08-29 09:56:01 +0100

    Begin

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -25,8 +25,11 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 import { store } from './store'
 
 import { createWebHistory, createRouter } from "vue-router";
-import LoginComponent from "./components/auth-page/Login.component.vue";
+import LoginComponent from "./components/auth-page/Login.component.vue"
 import PageToolbar from "../js/Components/PageToolbar.vue"
+import TextField from "../js/Components/customizedComponents/TextField.vue"
+import Autocomplete from "../js/Components/customizedComponents/Autocomplete.vue"
+import DateRangePicker from "../js/Components/customizedComponents/DateRangePicker.vue"
 import Btn from "../js/Components/Btn.vue";
 import Index from "./pages/welcome/Index.vue";
 
@@ -89,7 +92,7 @@ const vuetify = createVuetify({
         },
       },
     components: {
-        VDataTable,
+      VDataTable
     },
     directives,
     labsComponents,
@@ -117,6 +120,9 @@ createInertiaApp({
         .use(VueAxios, axios)
         .component('VueDatePicker', VueDatePicker)
         .component('page-toolbar', PageToolbar)
+        .component('text-field', TextField)
+        .component('autocomplete', Autocomplete)
+        .component('date-range-picker', DateRangePicker)
         .component('btn', Btn)
         .use(VueSweetalert2, options)
         .mount(el);
--- resources/js/pages/Emplois/Create.vue
+++ resources/js/pages/Emplois/Create.vue
@@ -1,10 +1,12 @@
 <script>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { router } from "@inertiajs/vue3";
+import { router, useForm } from "@inertiajs/vue3";
 import {
     mdiTimetable,
     mdiCheckCircle,
-    mdiCancel
+    mdiCancel,
+    mdiPlusCircle,
+    mdiCloseCircle
 } from '@mdi/js'
 export default {
     layout: AuthenticatedLayout,
@@ -14,16 +16,52 @@ export default {
             icon: {
                 mdiTimetable,
                 mdiCancel,
-                mdiCheckCircle
+                mdiCheckCircle,
+                mdiPlusCircle,
+                mdiCloseCircle
             },
-            date: null
+            date: null,
+            activeStep: 0,
+            daysOfWeek: ['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+            steps: ['Step 1', 'Step 2', 'Step 3'],
+            form: useForm({
+                seances: {},
+                section: null,
+                niveau: null,
+                classe: null
+            }),
+            items: Array.from({ length: 10 }).map((_, i) => ({
+                title: `Step ${i + 1}`,
+                subtitle: `Step ${i + 1} subtitle`,
+                value: i + 1,
+            })),
         }
     },
     methods: {
         goBack() {
             router.get(route('emplois.index'))
         },
-    }
+        addRow(day) {
+            this.form.seances[day].push({
+                horaire: null,
+                name: null,
+                before: null,
+                after: null,
+            });
+        },
+        removeRow(day, p) {
+            this.form.seances[day] = this.form.seances[day].filter((seance) => seance !== p);
+        },
+        submit(){
+            console.log(this.form)
+        }
+    },
+    mounted() {
+        this.daysOfWeek.forEach((day) => {
+            this.form.seances[day] = [];
+            this.addRow(day);
+        });
+    },
 }
 </script>
 <template>
@@ -34,62 +72,72 @@ export default {
                 <v-row>
                     <v-card-text>
                         <v-chip label variant="outlined" text-color="white" color="rgb(125, 0, 44, 0.75)" class="text-md-h6 green--text">Classe concerner</v-chip>
-                        <v-card outlined class="mb-md-2" height="600">
+                        <v-card outlined class="mb-md-2 auto-height-card">
                             <v-card-text>
-                                <v-row>
+                                <v-row dennse>
                                     <v-col md="4">
-                                        <v-select
+                                        <autocomplete
                                             label="Section"
-                                            :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
-                                        ></v-select>
+                                            v-model="form.section"
+                                            :items="['Primiere', 'College', 'Lycee', 'Superieur', 'Universite']"
+                                        ></autocomplete>
                                     </v-col>
                                     <v-col md="4">
-                                        <v-select
+                                        <autocomplete
                                             label="Niveau"
+                                            v-model="form.niveau"
                                             :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
-                                        ></v-select>
+                                        ></autocomplete>
                                     </v-col>
                                     <v-col md="4">
-                                        <v-select
+                                        <autocomplete
                                             label="Classe"
+                                            v-model="form.classe"
                                             :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
-                                        ></v-select>
+                                        ></autocomplete>
                                     </v-col>
-                                    <v-col md="12">
+                                    <v-col md="ei12">
                                         <VueDatePicker 
                                             v-model="date"
-                                            locale="fr"
                                             flow="calendar"
                                             date-picker
                                             range
                                             placeholder="Start Typing ..." />
                                     </v-col>
                                 </v-row>
-                                <v-stepper :items="['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi']">
-                                    <template v-slot:item.1>
-                                        <v-card title="Step Lundi" flat>...</v-card>
+                                <br>
+                                <!-- <v-stepper :items="['Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi']">
+                                    <template v-for="(day, i) in daysOfWeek" :key="i" >
+                                        <v-card :title="`Emploi du ${day}`" flat>
+                                            <v-card-text>
+                                                <v-row :key="seance.id" v-for="(seance, i) in form.seances[day]" dense>
+                                                    <v-col md="3">
+                                                        <VueDatePicker v-model="seance.horaire" time-picker range />
+                                                    </v-col>
+                                                    <v-col md="3">
+                                                        <text-field label="Matiere" placeholder="Matiere" v-model="seance.name"></text-field>
+                                                    </v-col>
+                                                    <v-col md="5">
+                                                        <autocomplete label="Salle" item-title="name" item-value="id" :items="fillieres" multiple chips v-model="seance.filliere">
+                                                        </autocomplete>
+                                                    </v-col>
+                                                    <v-col md="1">
+                                                        <v-btn variant="outlined" :disabled="!(form.seances.length > 1)" icon @click="removeRow(seance)" fab small color="error">
+                                                            <v-icon :icon="icon.mdiCloseCircle"></v-icon>
+                                                        </v-btn>
+                                                    </v-col>
+                                                </v-row>
+                                                <v-row dense>
+                                                    <v-col offset-md="11" md="1">
+                                                        <v-btn variant="outlined" icon @click="addRow(day)" fab small color="green">
+                                                            <v-icon :icon="icon.mdiPlusCircle"></v-icon>
+                                                        </v-btn>
+                                                    </v-col>
+                                                </v-row>
+                                            </v-card-text>
+                                        </v-card>
                                     </template>
-
-                                    <template v-slot:item.2>
-                                        <v-card title="Step Mardi" flat>...</v-card>
-                                    </template>
-
-                                    <template v-slot:item.3>
-                                        <v-card title="Step Mercredi" flat>...</v-card>
-                                    </template>
-
-                                    <template v-slot:item.4>
-                                        <v-card title="Step Jeudi" flat>...</v-card>
-                                    </template>
-
-                                    <template v-slot:item.5>
-                                        <v-card title="Step Vendredi" flat>...</v-card>
-                                    </template>
-
-                                    <template v-slot:item.6>
-                                        <v-card title="Step Samsedi" flat>...</v-card>
-                                    </template>
-                                </v-stepper>
+                                </v-stepper> -->
                             </v-card-text>
                         </v-card>
                     </v-card-text>
@@ -108,3 +156,9 @@ export default {
         </v-card-text>
     </v-card>
 </template>
+
+<style scoped>
+.auto-height-card {
+  height: auto; /* Cela permet à la carte d'ajuster automatiquement sa hauteur en fonction de son contenu */
+}
+</style>
