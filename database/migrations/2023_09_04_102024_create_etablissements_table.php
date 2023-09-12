ab5a921c5192576b13650afdaca9c0edba68475f
NanaAmadou <nana12amadou@gmail.com>
2023-09-12 09:02:59 +0100

    activationEts

--- database/migrations/2023_09_04_102024_create_etablissements_table.php
+++ database/migrations/2023_09_04_102024_create_etablissements_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('adresse');
             $table->json('telephone');
             $table->string('ville');
-            $table->boolean('statut')->default(0);
+            $table->boolean('statut');
             $table->string('logo')->nullable();
             $table->foreignIdFor(\App\Models\TypeEtablissement::class)->index()
                 ->references('id')->on('type_etablissements');
