dcde496f7fd12f5f70303f0150c04e2d182d133f
inz2020 <72919182+inz2020@users.noreply.github.com>
2023-09-12 11:06:02 +0100

    Fix conflicts retrieve

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,3 +30,38 @@ class DatabaseSeeder extends Seeder
         ]);
     }
 }
+
+
+
+// <?php
+
+// namespace Database\Seeders;
+
+// // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Seeder;
+// use Modules\Enseignement\Database\Seeders\EnseignementDatabaseSeeder;
+
+// class DatabaseSeeder extends Seeder
+// {
+//     /**
+//      * Seed the application's database.
+//      */
+//     public function run(): void
+//     {
+//         $this->call([
+//             PermissionSeeder::class,
+//             RoleSeeder::class,
+//             TypeEtablissementSeeder::class,
+//             SectionSeeder::class,
+//             NiveauTableSeeder::class
+//             // EtablissementsTableSeeder::class,
+//             // ClasseSeeder::class,
+//             // AnneeScolaireSeeder::class,
+//             // ClasseAnneeSeeder::class,
+//             // ApprenantSeeder::class,
+//             // SalleSeeder::class,
+//             // EtablissementSectionSeeder::class,
+//             // EnseignementDatabaseSeeder::class,
+//         ]);
+//     }
+// }
