106a7af2d8b42de5050e9820159d1daa2214012a
ismaelnouri <nouri.ismael@yahoo.com>
2023-11-20 14:50:51 +0100

    r

--- resources/views/superieur/bulletin.blade.php
+++ resources/views/superieur/bulletin.blade.php
@@ -97,139 +97,31 @@
         <div style="position:absolute;margin-top:10px;margin-left:300px">
         <img style="max-width:50%; height:auto" src="logos/iat-logo.png" alt="Logo de l'entreprise">
         </div>
-        <div style="position:absolute;margin-top:10px;margin-left:500px">
+        <div style="position:absolute;margin-top:10px;margin-left:490px">
         <b><i style="font-size: 13px; margin: 5px;">ANNEE SCOLAIRE : 2023-2024</i>&nbsp;&nbsp;</b><br>
-                    <b><i style="font-size: 13px; margin: 5px;">SEMESTRE : I</i>&nbsp;&nbsp;</b><br>
-                    <b><i style="font-size: 13px; margin: 5px;">SECTION : Supérieur</i>&nbsp;&nbsp;</b><br>
-                    <b><i style="font-size: 13px; margin: 5px;">NIVEAU : 1er Cycle 1ère année </i>&nbsp;&nbsp;</b><br>
-                    <b><i style="font-size: 13px; margin: 5px;">FILIERE : Informatique de Gestion </i>&nbsp;&nbsp;</b><br>
+            <b><i style="font-size: 13px; margin: 5px;">SEMESTRE : I</i>&nbsp;&nbsp;</b><br>
+            <b><i style="font-size: 13px; margin: 5px;">SECTION : Supérieur</i>&nbsp;&nbsp;</b><br>
+            <b><i style="font-size: 13px; margin: 5px;">NIVEAU : 1er Cycle 1ère année </i>&nbsp;&nbsp;</b><br>
+            <b><i style="font-size: 13px; margin: 5px;">FILIERE : Informatique de Gestion </i>&nbsp;&nbsp;</b><br>
         </div>
     </div>
                
 
-        <!-- titre du bulletin -->
-        <div style="background-color: grey;margin-left: 250px; width: 210px; height: 30px; margin-top: 150px;">
-            <b style="font-size: 20px;">BULLETIN DE NOTES</b> 
-        </div>
-        <!-- <div style="margin-left: 260px; width: 210px; height: 30px; margin-top: 10px;">
-            <b style="font-size: 20px;">Classe de : test</b> 
-        </div> -->
-        <!-- fin titre du bulletin -->
+    <!-- titre du bulletin -->
+    <div style="background-color: grey;margin-left: 250px; width: 210px; height: 30px; margin-top: 150px;">
+        <b style="font-size: 20px;">BULLETIN DE NOTES</b> 
+    </div>
+    
 
-        <!-- Informations sur le professeur et rang -->
-        <div style="margin-top: 10px; position:absolute">
-            <!-- <b style="font-size: 13px;">Prof responsable de la classe : <span style="color:green">NON DEFINI</span></b> <br> -->
-            <b style="font-size: 13px;">Nom et Prénom de l'étudiant : Ismael Nouri</b> <br>
-            <b style="font-size: 13px;">Moyenne obtenue : 14 / 20</b>
-        </div>
+    <div style="margin-top: 10px; position:absolute">
+        <!-- <b style="font-size: 13px;">Prof responsable de la classe : <span style="color:green">NON DEFINI</span></b> <br> -->
+        <b style="font-size: 13px;">Nom et Prénom de l'étudiant :</b> Ismael Nouri <br>
+        <b style="font-size: 13px;">Moyenne obtenue : 14 / 20</b>
+    </div>
+
+    
        
-        <!-- <div style="margin-top: 10px; position:absolute ;margin-left:460px;">
-            <b style="font-size: 13px;">Rang : 1er</b>
-        </div> -->
-       
-        <!-- Fin informations sur le professeur et rang -->
-
-        <!-- Debut du tableau -->
-        <table style="margin-top: 100px; font-size: 13px; width:100%">
-            <thead>
-                <tr style="text-align:center;" valign="center">
-                    <th style="text-align: left;"><b>Disciplines</b></th>
-                    <th width="30">MC<br>/20</th>
-                    <th width="30">NC<br>/20</th>
-                    <th width="30">MG<br>/20</th>
-                    <th width="35">Coef</th>
-                    <th width="40">Moy ceof</th>
-                    <th width="35">Rang</th>
-                    <th width="70">Appréciation</th>
-                    <th width="100">Signature</th>
-                </tr>
-            </thead>
-            <tbody>
-               
-                    <tr>
-                        <td><b>Mathematique</b></td>
-                        <td style="text-align:center;" width="30">{{$line->note_de_classe}}</td>
-                        <td style="text-align:center;" width="30">{{$line->note_de_composition}}</td>
-                        <td style="text-align:center;" width="30">{{$line->moyenne}}</td>
-                        <td style="text-align:center;" width="35">{{$line->coefficient}}</td>
-                        <td style="text-align:center;" width="40">{{$line->moyenne_coefficiente}}</td>
-                        <td style="text-align:center;" width="35"></td>
-                       
-                        <td style="text-align:center;" width="100"></td>
-                    </tr>
-
-                    <tr>
-                        <td><b>Mathematique</b></td>
-                        <td style="text-align:center;" width="30">{{$line->note_de_classe}}</td>
-                        <td style="text-align:center;" width="30">{{$line->note_de_composition}}</td>
-                        <td style="text-align:center;" width="30">{{$line->moyenne}}</td>
-                        <td style="text-align:center;" width="35">{{$line->coefficient}}</td>
-                        <td style="text-align:center;" width="40">{{$line->moyenne_coefficiente}}</td>
-                        <td style="text-align:center;" width="35"></td>
-                       
-                        <td style="text-align:center;" width="100"></td>
-                    </tr>
-
-                    <tr>
-                        <td><b>Mathematique</b></td>
-                        <td style="text-align:center;" width="30">{{$line->note_de_classe}}</td>
-                        <td style="text-align:center;" width="30">{{$line->note_de_composition}}</td>
-                        <td style="text-align:center;" width="30">{{$line->moyenne}}</td>
-                        <td style="text-align:center;" width="35">{{$line->coefficient}}</td>
-                        <td style="text-align:center;" width="40">{{$line->moyenne_coefficiente}}</td>
-                        <td style="text-align:center;" width="35"></td>
-                       
-                        <td style="text-align:center;" width="100"></td>
-                    </tr>
-              
-            
-                
-            </tbody>
-        </table>
-        <!-- Fin du tableau -->
-        
-        <!-- <div  style="background-image:url(test.jpg);width: 200px; padding-top:-400px;background-repeat:no-repeat;background-position: center center;filter:alpha(opacity=50);opacity: 0.2;-moz-opacity:0.5">
-        </div> -->
-        
-        <table style="font-size: 13px; margin-top: 10px;">
-            <tr style="text-align:center;">
-                <th width="153" colspan=3>Conduite</th>
-                <th width="153" colspan=3>Travail</th>
-                <th width="153" colspan=3>Tableau d'honneur</th>
-                <th width="153" colspan=3>Assiduité / Retards</th>
-            </tr>
-           
-            <tr>    
-                <td width="153"  height="20" colspan=3>
-                    <input type="checkbox"><label>Bonne</label><br>
-                    <input type="checkbox"><label>Avertissement</label><br>
-                    <input type="checkbox"><label>Blâme</label><br>
-                    <label></label><br>
-                    <label></label><br>
-                </td>
-                <td width="153"  height="20" colspan=3>
-                    <input type="checkbox" checked="checked"><label>Bien</label><br>
-                </td>
-                <td width="153"  height="20" colspan=3>
-                    <input type="checkbox"><label>Inscrit(e)</label><br>
-                    <input type="checkbox"><label>Félicitation</label><br>
-                    <input type="checkbox"><label>Encouragement</label><br>
-                    <input type="checkbox"><label>Non inscrit(e)</label><br>
-                    <label></label><br>
-                </td>
-                <td style="text-align:center" valign="center" width="153"  height="20" colspan=3><span style="color:green">R.A.S </span> / <span style="color:red;">  2</span></td>
-            </tr>
-        </table>
-        <!-- <div style="margin-top: 5px;">
-            <b style="font-size: 13px;">Appréciation du proviseur&nbsp;</b> 
-        </div> -->
-        
-        <div style="text-align: left; margin-top:20; position:absolute;">
-            <b style="font-size: 13px; margin: 5px;">Appréciation du proviseur</b><br>
-        </div>
-        <div style="margin-left: 600px; position:absolute; margin-top:20">
-            <b style="font-size: 13px; margin: 5px;">Visa des parents</b><br>
-        </div>
+     
    
     
 </body>
