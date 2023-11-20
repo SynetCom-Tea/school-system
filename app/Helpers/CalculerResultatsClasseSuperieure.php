02a2a9641874a255725f2631bb02f64ac1ac42d4
Momlaouan <momlaouali@gmail.com>
2023-11-20 15:54:32 +0100

    resultats groube by eu

--- app/Helpers/CalculerResultatsClasseSuperieure.php
+++ app/Helpers/CalculerResultatsClasseSuperieure.php
@@ -9,6 +9,8 @@
 
 use App\Models\Classe;
 use App\Models\ClasseAnnee;
+use App\Models\HistoriqueBulletin;
+use App\Models\HistoriqueNote;
 use Illuminate\Support\Facades\DB;
 
 if (!function_exists('calculerResultatsClasseSuperieure')) {
@@ -34,6 +36,8 @@ if (!function_exists('calculerResultatsClasseSuperieure')) {
             ];
         }
         $resultatsClasseAvecRang = calculerRangApprenants($resultatsClasse);
+        $moyenneClasse = calculerMoyenneClasse($resultatsClasse);
+
         // dd($resultatsClasseAvecRang);
         return $resultatsClasseAvecRang;
     }
@@ -75,5 +79,41 @@ if (!function_exists('calculerMoyenneClasse')) {
         $moyenneClasse = ($nombreApprenants > 0) ? ($totalMoyenneGenerale / $nombreApprenants) : 0;
 
         return $moyenneClasse;
+    }
+}
+
+
+if (!function_exists('ajouterHistoriqueBulletin')) {
+    function ajouterHistoriqueBulletin($resultat) {
+        $historiqueBulletin = HistoriqueBulletin::create([
+            'apprenant_id' => $resultat['apprenant'],
+            'nom_classe' => $resultat['nom_classe'],
+            'periode' => $resultat['periode'],
+            'classe_annee_id' => $resultat['classe'],
+            'matricule_apprenant' => $resultat['matricule_apprenant'],
+            'nom_prenom_apprenant' => $resultat['nom_apprenant'] . ' ' . $resultat['prenom_apprenant'],
+            'moyenne_details_notes' => $resultat['moyenne'],
+            'total_coefficient' => $resultat['total_coefficient'],
+            'somme_note_generale' => $resultat['somme_note_generale'],
+            'somme_note_generale_coefficient' => $resultat['somme_note_generale_coefficient'],
+            'total_volume_horaire' => $resultat['total_volume_horaire'],
+            'rang' => $resultat['rang'],
+        ]);
+
+        foreach ($resultat['details_notes'] as $detailNote) {
+            HistoriqueNote::create([
+                'historique_bulletin_id' => $historiqueBulletin->id,
+                'nom_eu' => $detailNote['nom_eu'],
+                'nom_matiere' => $detailNote['nom_matiere'],
+                'coefficient' => $detailNote['coefficient_matiere'],
+                'volume_horaire_matiere' => $detailNote['volume_horaire_matiere'],
+                'note_origine_devoir' => $detailNote['note_origine_devoir'],
+                'note_origine_examen' => $detailNote['note_origine_examen'],
+                'note_devoir_pourcentage' => $detailNote['note_devoir'],
+                'note_examen_pourcentage' => $detailNote['note_examen'],
+                'note_generale' => $detailNote['note_generale'],
+                'note_generale_coefficiente' => $detailNote['note_generale_coefficiente'],
+            ]);
+        }
     }
 }
