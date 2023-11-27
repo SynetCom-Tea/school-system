40ee786ed75a7ac8712a2f518c745de10036bcea
Momlaouan <momlaouali@gmail.com>
2023-11-27 10:15:13 +0100

    generation

--- resources/js/pages/Rapport/Generation.vue
+++ resources/js/pages/Rapport/Generation.vue
@@ -154,22 +154,16 @@ export default {
                 color="primary"
             >
                 <v-tab value="option-1">
-                <v-icon start>
-                    {{ icons.mdiChartDonut }}
-                </v-icon>
-                Par Semestre
-                </v-tab>
+                    <v-icon start>
+                        {{ icons.mdiChartDonut }}
+                    </v-icon>
+                    Par periode
+                    </v-tab>
                 <v-tab value="option-2">
-                <v-icon start>
-                    {{ icons.mdiRepeatVariant }}
-                </v-icon>
-                Par {{ apprenant }}
-                </v-tab>
-                <v-tab value="option-3">
-                <v-icon start>
-                    {{ icons.mdiRepeat }}
-                </v-icon>
-                Option 3
+                    <v-icon start>
+                        {{ icons.mdiRepeatVariant }}
+                    </v-icon>
+                    Par {{ apprenant }}
                 </v-tab>
             </v-tabs>
             <v-window v-model="tab">
@@ -310,19 +304,6 @@ export default {
                         </p>
                         </v-card-text>
                     </v-card>
-                </v-window-item>
-                <v-window-item value="option-3">
-                <v-card flat>
-                    <v-card-text>
-                    <p>
-                        Fusce a quam. Phasellus nec sem in justo pellentesque facilisis. Nam eget dui. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. In dui magna, posuere eget, vestibulum et, tempor auctor, justo.
-                    </p>
-
-                    <p class="mb-0">
-                        Cras sagittis. Phasellus nec sem in justo pellentesque facilisis. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nam at tortor in tellus interdum sagittis.
-                    </p>
-                    </v-card-text>
-                </v-card>
                 </v-window-item>
             </v-window>
         </div>
