d1ea055267b0640c9062bfc41fe97c70c914a989
ismaelnouri <nouri.ismael@yahoo.com>
2023-11-30 09:46:10 +0100

    primaire bul

--- resources/views/recu_versement.blade.php
+++ resources/views/recu_versement.blade.php
@@ -58,7 +58,7 @@
             @if($etablissement->logo === null)
             <img style="max-width:30%; height: 11%;" src="logos/iat-logo.png" alt="Logo de l'entreprise">
             @else
-            <img style="max-width:30%; height:auto" src="logos/{{$etablissement->logo}}" alt="Logo de l'entreprise">
+            <img style="max-width:30%; height:11%" src="logos/{{$etablissement->logo}}" alt="Logo de l'entreprise">
             @endif
         </div>
         <p style="margin-bottom: 20px; text-align:center; margin-top:-120px;">
