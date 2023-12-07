16dd7e72c2adbb7c043fae15f8d39790bd7052c7
Abdoul7473 <maroukimbaabdoulaziz123@gmail.com>
2023-12-07 12:13:08 +0100

    hyuj

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,75 +17,75 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            RegimeValidationSeeder::class,
-            CycleSeeder::class,
-            PermissionSeeder::class,
-            TypeEtablissementSeeder::class,
-            SectionSeeder::class,
-            TypeDocumentSeeder::class,
-            NiveauTableSeeder::class,
-            // EtablissementsTableSeeder::class,
-            // EtablissementSectionTableSeeder::class,
-            // ClassesTableSeeder::class,
-            AnneeScolaireSeeder::class,
-            // ClasseAnneesTableSeeder::class,
-            // TuteursSeeder::class,
-            // ApprenantsTableSeeder::class,
-            // EnseignantsTableSeeder::class,
-            TypeFraisSeeder::class,
-            // FraisSeeder::class,
-            RoleSeeder::class,
-            // InscriptionsTableSeeder::class,
-            // VersementsTableSeeder::class,
-            // ApprenantClasseAnneesTableSeeder::class,
-            // SallesTableSeeder::class,
-            // MatieresTableSeeder::class,
-            // NiveauMatieresTableSeeder::class,
+        // $this->call([
+        //     RegimeValidationSeeder::class,
+        //     CycleSeeder::class,
+        //     PermissionSeeder::class,
+        //     TypeEtablissementSeeder::class,
+        //     SectionSeeder::class,
+        //     TypeDocumentSeeder::class,
+        //     NiveauTableSeeder::class,
+        //     // EtablissementsTableSeeder::class,
+        //     // EtablissementSectionTableSeeder::class,
+        //     // ClassesTableSeeder::class,
+        //     AnneeScolaireSeeder::class,
+        //     // ClasseAnneesTableSeeder::class,
+        //     // TuteursSeeder::class,
+        //     // ApprenantsTableSeeder::class,
+        //     // EnseignantsTableSeeder::class,
+        //     TypeFraisSeeder::class,
+        //     // FraisSeeder::class,
+        //     RoleSeeder::class,
+        //     // InscriptionsTableSeeder::class,
+        //     // VersementsTableSeeder::class,
+        //     // ApprenantClasseAnneesTableSeeder::class,
+        //     // SallesTableSeeder::class,
+        //     // MatieresTableSeeder::class,
+        //     // NiveauMatieresTableSeeder::class,
 
-            // FraisSeeder::class,
-            // VersementsSeeder::class,
-            // ApprenantClasseAnneesSeeder::class,
-             PeriodeTableSeeder::class,
-            TypeEvaluationTableSeeder::class,
-            SystemeLmdsTableSeeder::class,
-            RegimeEvaluationsTableSeeder::class,
-            // EvaluationsTableSeeder::class,
-            // EnseignantTableSeeder::class,
-            // EnseignementAnneesTableSeeder::class,
-            // EnseignantMatieresTableSeeder::class,
-            // EnseignantSeeder::class,
-            // EnseignantMatiereSeeder::class,
-        ]);
+        //     // FraisSeeder::class,
+        //     // VersementsSeeder::class,
+        //     // ApprenantClasseAnneesSeeder::class,
+        //      PeriodeTableSeeder::class,
+        //     TypeEvaluationTableSeeder::class,
+        //     SystemeLmdsTableSeeder::class,
+        //     RegimeEvaluationsTableSeeder::class,
+        //     // EvaluationsTableSeeder::class,
+        //     // EnseignantTableSeeder::class,
+        //     // EnseignementAnneesTableSeeder::class,
+        //     // EnseignantMatieresTableSeeder::class,
+        //     // EnseignantSeeder::class,
+        //     // EnseignantMatiereSeeder::class,
+        // ]);
         // $this->call(ModelHasRolesTableSeeder::class);
         // $this->call(ModelHasPermissionsTableSeeder::class);
         // $this->call(SystemeLmdsTableSeeder::class);
-        // $this->call(EtablissementsTableSeeder::class);
-        // $this->call(EtablissementSectionTableSeeder::class);
-        // $this->call(ParametresTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(EtablissementTypeFraisTableSeeder::class);
-        // $this->call(EtablissementTypeDocumentsTableSeeder::class);
-        // $this->call(SallesTableSeeder::class);
-        // $this->call(MatieresTableSeeder::class);
-        // $this->call(NiveauMatieresTableSeeder::class);
-        // $this->call(UesTableSeeder::class);
-        // $this->call(FilieresTableSeeder::class);
-        // $this->call(CycleFilieresTableSeeder::class);
-        // $this->call(FiliereNiveauMatiereUesTableSeeder::class);
-        // $this->call(ClassesTableSeeder::class);
-        // $this->call(ClasseAnneesTableSeeder::class);
-        // $this->call(FraisTableSeeder::class);
-        // $this->call(EnseignantsTableSeeder::class);
-        // $this->call(EnseignementAnneesTableSeeder::class);
-        // $this->call(ApprenantsTableSeeder::class);
-        // $this->call(TuteursTableSeeder::class);
-        // $this->call(ApprenantClasseAnneesTableSeeder::class);
-        // $this->call(ApprenantTuteursTableSeeder::class);
-        // $this->call(InscriptionsTableSeeder::class);
-        // $this->call(VersementsTableSeeder::class);
-        // $this->call(EvaluationsTableSeeder::class);
-        // $this->call(NotesTableSeeder::class);
+        $this->call(EtablissementsTableSeeder::class);
+        $this->call(EtablissementSectionTableSeeder::class);
+        $this->call(ParametresTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(EtablissementTypeFraisTableSeeder::class);
+        $this->call(EtablissementTypeDocumentsTableSeeder::class);
+        $this->call(SallesTableSeeder::class);
+        $this->call(MatieresTableSeeder::class);
+        $this->call(NiveauMatieresTableSeeder::class);
+        $this->call(UesTableSeeder::class);
+        $this->call(FilieresTableSeeder::class);
+        $this->call(CycleFilieresTableSeeder::class);
+        $this->call(FiliereNiveauMatiereUesTableSeeder::class);
+        $this->call(ClassesTableSeeder::class);
+        $this->call(ClasseAnneesTableSeeder::class);
+        $this->call(FraisTableSeeder::class);
+        $this->call(EnseignantsTableSeeder::class);
+        $this->call(EnseignementAnneesTableSeeder::class);
+        $this->call(ApprenantsTableSeeder::class);
+        $this->call(TuteursTableSeeder::class);
+        $this->call(ApprenantClasseAnneesTableSeeder::class);
+        $this->call(ApprenantTuteursTableSeeder::class);
+        $this->call(InscriptionsTableSeeder::class);
+        $this->call(VersementsTableSeeder::class);
+        $this->call(EvaluationsTableSeeder::class);
+        $this->call(NotesTableSeeder::class);
     }
 }
 
